ca819766626da666f3db3315502592af87b84568
freshstartagain <newstartagain@gmail.com>
2020-04-03 17:27:58 +0800

    Finished footer

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,22 +1,13 @@
 document.addEventListener('DOMContentLoaded',()=> {
-    const hero_p = document.getElementById('hero-p');
+   
+    let year = new Date().getFullYear();
 
-    const welcome = "Welcome to my page :)";
-    const introduction = "I am Christian Lester Blanco Cayabyab, I am an experienced software developer."
-    const experience = "I have experience working on different kinds of organizations, from small businesses, startups, medical and banks. Solving problems and providing IT solutions.";
-
-    var typewriter = new Typewriter(hero_p, {
-        loop: true
-    });
-
-    typewriter.typeString(welcome)
-        .pauseFor(1000)
-        .deleteAll()
-        .typeString(introduction)
-        .pauseFor(1000)
-        .deleteAll()
-        .typeString(experience)
-        .start();
+    console.log(year);
     
+    const copyright_text = document.getElementById('copyright_text');
+
+    copyright_text.innerText = `Copyright ${year}. All rights reserved.`;
+
+
 });
 
