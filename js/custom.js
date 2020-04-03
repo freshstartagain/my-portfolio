9af473f385d48355d7b752b38d0e8561cb56465c
freshstartagain <newstartagain@gmail.com>
2020-04-03 17:50:32 +0800

    Updates

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,22 @@
 document.addEventListener('DOMContentLoaded',()=> {
-   
-    let year = new Date().getFullYear();
-
-    console.log(year);
     
-    const copyright_text = document.getElementById('copyright_text');
+    function set_copyright(){
+        let year = new Date().getFullYear();
+        const copyright_text = document.getElementById('copyright_text');
 
-    copyright_text.innerText = `Copyright ${year}. All rights reserved.`;
+        copyright_text.innerText = `Copyright ${year}. All rights reserved.`;
+    }
 
+    function set_scroll(){
+        var scroll = new SmoothScroll('.navigation a[href*="#"]', {
+            speed: 800,
+            speedAsDuration: true
+        });
+    }
+    
+    set_copyright();
+    set_scroll();
 
+    
 });
 
